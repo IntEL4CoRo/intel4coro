5b645ac5ecf342380ff009cfa4959e0a9332cae0
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-03-03 13:17:00 +0100

    Update launch files and demo package

--- cram/with_jupyter/cram_launch.sh
+++ cram/with_jupyter/cram_launch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /home/workspace/ros/devel/setup.bash
-roslaunch cram_pick_place_tutorial world.launch &
+# roslaunch cram_pick_place_tutorial world.launch &
+roslaunch cram_projection_demos household_pr2.launch &
+
 # export THIS_IP=$(ifconfig 'docker0' | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*')
 sleep 2
 echo ""
