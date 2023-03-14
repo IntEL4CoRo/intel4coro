49d6e5364ee94252c49d2b165ebe5e8b36e97b41
Yanxiang Zhan <yanxiang@uni-bremen.de>
2023-03-14 15:25:15 +0100

    kubernetes configs and dockerfiles for jupyterhub

--- cram/base_jupyter/kubernetes_config/commands.sh
+++ cram/base_jupyter/kubernetes_config/commands.sh
@@ -12,12 +12,18 @@ helm upgrade --cleanup-on-fail \
   --namespace cram \
   --version=2.0.0 \
   --values config.yaml
+  
+# Delete a helm namespace
+helm delete cram --namespace cram
+kubectl delete namespace cram
 
 # Set default kubernetes namespace to cram
 microk8s.kubectl config set-context $(kubectl config current-context) --namespace cram
 
 # Monitor kubernetes pods of default namespace
-watch microk8s.kubectl get pod --namepsace cram
+watch microk8s.kubectl get pod --namespace cram
+
+# Delete a pod
 kubectl delete pod jupyter-admin 
 
 # Get all service
@@ -28,6 +34,8 @@ microk8s.kubectl get service proxy-public
 
 # Forward the JupyterHub service to localhost:8080
 microk8s.kubectl port-forward service/proxy-public 8080:http
+
+microk8s kubectl expose service/proxy-public --type=LoadBalancer --name=proxy-public-lb --external-ip=192.168.102.13
 
 # Output logs of a node
 microk8s.kubectl logs jupyter-admin
